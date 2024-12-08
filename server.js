5537829b9462420234ce14f9e866d05c880fd080
SamuelLRuan <duytt2k@gmail.com>
2024-12-08 11:17:23 +0800

    finish add, delete, call product list from database

--- server.js
+++ server.js
@@ -22,9 +22,17 @@ const db = new pg.Client({
 });
 db.connect();
 
+const corsOption = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOption));
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
@@ -44,7 +52,6 @@ app.get('/api/products', async (req, res) => {
 //get products on shop page
 app.get('/api/shop/products', async (req, res) => {
   const { sortType, page, limit } = req.query;
-  console.log({ sortType, page, limit });
   let result = [];
 
   try {
@@ -127,6 +134,172 @@ app.get('/api/user/info/:userId', async (req, res) => {
     }
   }
 });
+
+//get product list in cart
+app.get('/api/cart', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+
+    //passed compare -> get user info
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      const cart = await db.query(
+        `
+        SELECT cart.cart_id,products.id, products.name, products.price, products.image1, cart.product_size, cart.user_id, users.username, users.email, users.phonenumber, users.address FROM products
+        JOIN cart
+        ON products.id = cart.product_id  
+        JOIN users
+        ON users.id = cart.user_id
+        WHERE user_id=$1
+        ORDER BY product_id ASC;
+        `,
+        [payload.sub]
+      );
+      const data = cart.rows;
+      return res.status(201).json({ data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+//get product list in compare
+app.get('/api/compare', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+
+    //passed compare -> get user info
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      const cart = await db.query(
+        `
+        SELECT compare.compare_id,products.id, products.name, products.price, products.image1, compare.product_size FROM products
+        JOIN compare
+        ON products.id = compare.product_id  
+        JOIN users
+        ON users.id = compare.user_id
+        WHERE user_id=$1
+        ORDER BY product_id ASC
+        `,
+        [payload.sub]
+      );
+      const data = cart.rows;
+      return res.status(201).json({ data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+//get product list in wishlist
+app.get('/api/wishlist', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+
+    //passed compare -> get user info
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      const cart = await db.query(
+        `
+        SELECT wishlist.wishlist_id,products.id, products.name, products.price, products.image1, wishlist.product_size FROM products
+        JOIN wishlist
+        ON products.id = wishlist.product_id  
+        JOIN users
+        ON users.id = wishlist.user_id
+        WHERE user_id=$1
+        ORDER BY product_id ASC
+        `,
+        [payload.sub]
+      );
+      const data = cart.rows;
+      return res.status(201).json({ data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
 //register route
 app.post('/api/register', async (req, res) => {
   const user = req.body;
@@ -211,6 +384,228 @@ app.post('/api/login', async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+//add product to cart
+app.post('/api/cart', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+
+      const data = req.body;
+
+      await db.query(
+        'INSERT INTO cart(user_id, product_id, product_size, quantity) VALUES($1, $2, $3, $4)',
+        [user.id, data.productId, data.size, data.quantity]
+      );
+      return res.status(201).json({ msg: 'Add to cart successfully' });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+//add product to compare
+app.post('/api/compare', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+
+      const data = req.body;
+
+      await db.query(
+        'INSERT INTO compare(user_id, product_id, product_size, quantity) VALUES($1, $2, $3, $4)',
+        [user.id, data.productId, data.size, data.quantity]
+      );
+      return res.status(201).json({ msg: 'Add to compare successfully' });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+//add product to wishlist
+app.post('/api/wishlist', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+
+      const data = req.body;
+
+      await db.query(
+        'INSERT INTO wishlist(user_id, product_id, product_size, quantity) VALUES($1, $2, $3, $4)',
+        [user.id, data.productId, data.size, data.quantity]
+      );
+      return res.status(201).json({ msg: 'Add to wishlist successfully' });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+//move product from wishlist to cart
+app.post('/api/wishlist/move_to_cart', async (req, res) => {
+  //check token
+  const token = req.headers.authorization?.slice(7);
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const [encodeHeader, encodePayload, tokenSignature] = token.split('.');
+  const tokenData = `${encodeHeader}.${encodePayload}`;
+  const hmac = crypto.createHmac('sha256', jwtSecret);
+  const signature = hmac.update(tokenData).digest('base64url');
+
+  //compare token signature
+  if (signature === tokenSignature) {
+    const payload = JSON.parse(atob(encodePayload));
+    try {
+      const result = await db.query('SELECT * FROM users WHERE id=$1', [
+        payload.sub
+      ]);
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+      if (payload.exp < Date.now()) {
+        return res.status(401).json({
+          message: 'Unauthorized'
+        });
+      }
+
+      await db.query(
+        `INSERT INTO cart(user_id, product_id, product_size,quantity)
+          SELECT user_id, product_id, product_size,quantity
+          FROM wishlist
+          WHERE user_id = $1
+          `,
+        [payload.sub]
+      );
+
+      await db.query(
+        `DELETE FROM wishlist
+          WHERE user_id = $1
+          `,
+        [payload.sub]
+      );
+      return res.status(201).json({ msg: 'Add to compare successfully' });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+//delete item from cart
+app.delete('/api/cart/:cart_id', async (req, res) => {
+  const cartId = req.params.cart_id;
+  await db.query('DELETE FROM cart WHERE cart_id = $1', [parseInt(cartId)]);
+  return res.json('ok');
+});
+
+//delete item from compare
+app.delete('/api/compare/:compare_id', async (req, res) => {
+  const compareId = req.params.compare_id;
+  await db.query('DELETE FROM compare WHERE compare_id = $1', [
+    parseInt(compareId)
+  ]);
+  return res.json('ok');
+});
+
+//delete item from wishlist
+app.delete('/api/wishlist/:wishlist_id', async (req, res) => {
+  const wishlistId = req.params.wishlist_id;
+  await db.query('DELETE FROM wishlist WHERE wislist_id = $1', [
+    parseInt(wishlistId)
+  ]);
+  return res.json('ok');
 });
 
 app.listen(port, () => {
